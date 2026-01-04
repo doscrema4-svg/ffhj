0fc1aead507c801353b0d19e100a6e80c04d906a
doscrema4-svg <doscrema4@gmail.com>
2026-01-04 17:06:53 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1454134874670891235/KDeFBrrfBFbUANq_4eqVPhRbk6A9yA7SUr2mlxyWcAvgZB2FkJlZanka2dzgr2xTnvri'; // Reemplaza esto con tu Webhook real
